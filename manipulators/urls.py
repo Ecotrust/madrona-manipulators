d7ca9661f8905d3dd5b81de68cdc8e01829a517b
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-03 13:21:21 -0700

    updating for django 1.10, removing madrona deps

--- manipulators/urls.py
+++ manipulators/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from manipulators import views
 
-urlpatterns = patterns('manipulators.views',
-    (r'^test/$', 'testView'),
-    (r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', 'mpaManipulatorList'),
-    url(r'^([A-Za-z0-9_,]+)/$', 'multi_generic_manipulator_view', name='manipulate'),
-    url(r'^$', 'multi_generic_manipulator_view', {'manipulators': None}, name='manipulate-blank'),
-    url(r'^/$', 'multi_generic_manipulator_view', {'manipulators': None}, name='manipulate-blank'),
-)
+urlpatterns = [
+    'manipulators.views',
+    # (r'^test/$', views.testView),
+    (r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
+    url(r'^([A-Za-z0-9_,]+)/$', views.multi_generic_manipulator_view, name='manipulate'),
+    url(r'^$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
+    url(r'^/$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
+]
