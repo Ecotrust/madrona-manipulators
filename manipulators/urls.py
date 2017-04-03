845c780d2726807d6f4ddd05f3cc7a1b4d869d21
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-03 14:25:44 -0700

    more updates for django 1.10

--- manipulators/urls.py
+++ manipulators/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from manipulators import views
 
 urlpatterns = [
-    'manipulators.views',
+    # 'manipulators.views',
     # (r'^test/$', views.testView),
-    (r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
+    url(r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
     url(r'^([A-Za-z0-9_,]+)/$', views.multi_generic_manipulator_view, name='manipulate'),
     url(r'^$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
     url(r'^/$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
