ebcb9bba5ba2c24e4c0d0cd60e1054f334dd6968
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-18 15:57:26 -0700

    newer django tells me I should remove this

--- manipulators/urls.py
+++ manipulators/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
     url(r'^([A-Za-z0-9_,]+)/$', views.multi_generic_manipulator_view, name='manipulate'),
     url(r'^$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
-    url(r'^/$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
 ]
