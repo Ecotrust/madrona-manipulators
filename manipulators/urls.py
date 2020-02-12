f6018258856594e1cc128953fd7dcaf1325bb264
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-02-12 10:44:11 -0800

    testView view was removed 3 years ago. py3 dj2/3 overhaul.

--- manipulators/urls.py
+++ manipulators/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #'manipulators.views',
-    re_path(r'^test/$', views.testView),
+    # re_path(r'^test/$', views.testView),
     re_path(r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
     re_path(r'^([A-Za-z0-9_,]+)/$', views.multi_generic_manipulator_view, name='manipulate'),
     re_path(r'^$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
